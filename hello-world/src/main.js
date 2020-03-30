aa7bbcbd06da7deba63cad0727a10108be6e1f2a
TsuNiWan <tom007880510@gmail.com>
2020-03-31 00:47:37 +0800

    install bootstrap & axios

--- hello-world/src/main.js
+++ hello-world/src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
 
 new Vue({
   router,
