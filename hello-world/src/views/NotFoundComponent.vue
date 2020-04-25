ced29cf3ddf964345b027ff798f3169946987170
TsuNiWan <tom007880510@gmail.com>
2020-04-25 17:30:47 +0800

    Create NotFoundComponent.vue

---
+++ hello-world/src/views/NotFoundComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container">
+    <div class="alert alert-danger" role="alert">
+      <p class="text-center my-2 py-2">
+        <span>很抱歉！您搜尋的頁面不存在！</span>
+      </p>
+    </div>
+  </div>
+</template>
