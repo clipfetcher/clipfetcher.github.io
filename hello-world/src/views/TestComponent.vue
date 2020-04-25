dbee674b57080ab74921aaf309fb93ddb19f1b4b
TsuNiWan <tom007880510@gmail.com>
2020-04-25 19:42:39 +0800

    Create TestComponent.vue

---
+++ hello-world/src/views/TestComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <HighlightList></HighlightList>
+  </div>
+</template>
+
+<script>
+import HighlightList from "@/components/HighlightList.vue";
+
+export default {
+  name: "test",
+  data() {
+    return {};
+  },
+  components: { HighlightList }
+};
+</script>
