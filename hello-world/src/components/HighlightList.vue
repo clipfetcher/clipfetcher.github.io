0a1a1e7b5284e7ed3baf0d1162cf92c78a0aa9a1
TsuNiWan <tom007880510@gmail.com>
2020-05-10 20:43:47 +0800

    Add HighlightList with modal appraise & source link figure

--- hello-world/src/components/HighlightList.vue
+++ hello-world/src/components/HighlightList.vue
@@ -1,52 +1,113 @@
 <template>
   <!-- 精華影片顯示 -->
-  <div class="card-body">
+  <div class="m-4">
+    <transition
+      @enter="startTransitionModal"
+      @after-enter="endTransitionModal"
+      @before-leave="endTransitionModal"
+      @after-leave="startTransitionModal"
+    >
+      <div class="modal fade" v-if="showModal" ref="modal">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button class="close" type="button" @click="showModal = !showModal">
+                <span aria-hidden="true">×</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <form @submit.prevent="appraise">
+                <div class="form-group">
+                  <label for="validationText">評論：</label>
+                  <br />
+                  <button
+                    type="button"
+                    class="btn m-1"
+                    :class="notAccurate ? 'btn-secondary' : 'btn-outline-secondary'"
+                    @click="textButton('notAccurate')"
+                  >影片不精準</button>
+                  <button
+                    type="button"
+                    class="btn m-1"
+                    :class="videoLong ? 'btn-secondary' : 'btn-outline-secondary'"
+                    @click="textButton('videoLong')"
+                  >影片長度過長</button>
+                  <button
+                    type="button"
+                    class="btn m-1"
+                    :class="analysisLong ? 'btn-secondary' : 'btn-outline-secondary'"
+                    @click="textButton('analysisLong')"
+                  >影片分析太久</button>
+                  <br />
+                  <input
+                    type="text"
+                    class="form-control"
+                    :class="validationText?'is-invalid':''"
+                    id="validationText"
+                    @change="checkText"
+                    @keyup="checkText"
+                    v-model="text"
+                  />
+                  <div class="invalid-feedback">評論不得為空</div>
+                </div>
+                <div class="form-group">
+                  <label for="starRating">分數：</label>
+                  <br />
+                  <template v-for="index in 5">
+                    <i
+                      v-if="index <= starRating"
+                      :key="index.id"
+                      class="text-warning fas fa-star"
+                      @click="clickRating(index)"
+                      @mouseover="mouseOverRrating(index)"
+                      @touchstart="mouseOverRrating(index)"
+                      @mouseout="mouseOutRrating"
+                      @touchend="mouseOutRrating"
+                    ></i>
+                    <i
+                      v-else
+                      :key="index.id"
+                      class="text-warning far fa-star"
+                      @click="clickRating(index)"
+                      @mouseover="mouseOverRrating(index)"
+                      @touchstart="mouseOverRrating(index)"
+                      @mouseout="mouseOutRrating"
+                      @touchend="mouseOutRrating"
+                    ></i>
+                  </template>
+                  {{ starRating }}
+                </div>
+                <button type="submit" class="btn btn-primary float-right">送出</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </transition>
+    <div class="modal-backdrop fade d-none" ref="backdrop"></div>
     <div class="row">
-      <div class="col-12 col-md-4">
+      <div class="col-12 col-md-4 my-2">
         <div class="embed-responsive embed-responsive-16by9">
           <iframe class="embed-responsive-item" :src="youtube_embed" allowfullscreen></iframe>
         </div>
       </div>
-      <div class="col-12 col-md-3">
-        <span>{{ vod_id }}</span>
+      <div class="col-12 col-md-8">
+        <a
+          :href="'https://www.twitch.tv/videos/' + vod_id"
+          target="_blank"
+          class="card-link float-right"
+        >
+          <i class="fas fa-link fa-lg" data-toggle="tooltip" title="VOD網址"></i>
+        </a>
+        <p class="text-left m-0">ID：{{ vod_id }}</p>
         <p class="text-left m-0">實況主：{{ channel_id }}</p>
         <!--<p class="text-left m-0">標題：</p>-->
         <p class="text-left m-0">遊戲分類：{{ game }}</p>
-      </div>
-      <div class="col-12 col-md-5 align-self-end">
-        <button type="button" class="btn btn-outline-secondary m-1">影片不精準</button>
-        <button type="button" class="btn btn-outline-secondary m-1">影片長度過長</button>
-        <button type="button" class="btn btn-outline-secondary m-1">影片分析太久</button>
-        <button type="button" class="btn btn-outline-secondary m-1">其他</button>
-        <br />
-        <div class="float-right">
-          <template v-for="index in 5">
-            <i
-              v-if="index <= starRating"
-              :key="index.id"
-              class="text-warning fas fa-star"
-              @click="clickRating(index)"
-              @mouseover="mouseOverRrating(index)"
-              @touchstart="mouseOverRrating(index)"
-              @mouseout="mouseOutRrating"
-              @touchend="mouseOutRrating"
-            ></i>
-            <i
-              v-else
-              :key="index.id"
-              class="text-warning far fa-star"
-              @click="clickRating(index)"
-              @mouseover="mouseOverRrating(index)"
-              @touchstart="mouseOverRrating(index)"
-              @mouseout="mouseOutRrating"
-              @touchend="mouseOutRrating"
-            ></i>
-          </template>
-          {{ starRating }}
-          {{ avg_score }}
-        </div>
+        <p class="text-left m-0">目前分數：{{ avg_score }}</p>
+        <button class="btn btn-outline-info float-right m-1" @click="showModal = !showModal">我要評價</button>
       </div>
     </div>
+    <hr class="my-4" />
   </div>
 </template>
 
@@ -56,7 +117,16 @@ export default {
   props: ["vod_id", "channel_id", "game", "youtube_url", "avg_score"],
   data() {
     return {
-      starRating: 0
+      temp_starRating: 0,
+      starRating: 0,
+      text: "",
+      showModal: false,
+
+      notAccurate: false,
+      videoLong: false,
+      analysisLong: false,
+      validationText: false,
+      response: null
     };
   },
   methods: {
@@ -72,11 +142,63 @@ export default {
     clickRating: function(val) {
       this.starRating = this.temp_starRating = val;
       this.isRating = true;
+    },
+    startTransitionModal: function() {
+      this.$refs.backdrop.classList.toggle("d-block");
+      this.$refs.modal.classList.toggle("d-block");
+    },
+    endTransitionModal: function() {
+      this.$refs.backdrop.classList.toggle("show");
+      this.$refs.modal.classList.toggle("show");
+    },
+    textButton: function(button) {
+      let output = "";
+      if (button == "notAccurate") {
+        this.notAccurate = !this.notAccurate;
+      } else if (button == "videoLong") {
+        this.videoLong = !this.videoLong;
+      } else if (button == "analysisLong") {
+        this.analysisLong = !this.analysisLong;
+      }
+      if (this.notAccurate) {
+        output += " 影片不精準 ";
+      }
+      if (this.videoLong) {
+        output += " 影片長度過長 ";
+      }
+      if (this.analysisLong) {
+        output += " 影片分析太久 ";
+      }
+      this.text = output;
+    },
+    checkText: function() {
+      if (this.text == "" || this.text == null) {
+        this.validationText = true;
+      } else {
+        this.validationText = false;
+      }
+    },
+    appraise: function() {
+      if (this.text == "" || this.text == null) {
+        this.validationText = true;
+      } else {
+        this.validationText = false;
+        this.axios
+          .post("https://clip-fetcher.herokuapp.com/api/vod/appraise", {
+            highlight_id: this.vod_id,
+            text: this.text,
+            score: this.starRating
+          })
+          .then(response => (this.response = response))
+          .catch(function(error) {
+            console.log(error);
+          });
+        this.showModal = !this.showModal;
+      }
     }
   },
   computed: {
     youtube_embed: function() {
-      //https://youtu.be/frguLOUro2E
       let vodData = this.youtube_url;
       vodData = vodData.split("?");
       let vid = vodData[0];
