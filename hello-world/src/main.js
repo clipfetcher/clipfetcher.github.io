9d0a1cefd2fb05a1c519cfa0353ba82f468cae25
TsuNiWan <tom007880510@gmail.com>
2020-05-10 22:52:56 +0800

    Add module BootstrapVue

--- hello-world/src/main.js
+++ hello-world/src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+
 import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
+
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import './assets/my.css'
 
+Vue.use(BootstrapVue)
 Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
