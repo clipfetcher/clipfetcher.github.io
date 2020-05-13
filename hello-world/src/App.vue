313e9ac34375379ae982f3843daf75eb8aa6f106
TsuNiWan <tom007880510@gmail.com>
2020-05-14 01:24:52 +0800

    Add opinion validation

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -41,25 +41,27 @@
             <div class="form-group">
               <label for="mail">電子信箱：</label>
               <input
-                type="email"
+                type="text"
                 class="form-control"
+                :class="emailError?'is-invalid':''"
                 id="mail"
                 v-model="mail"
                 aria-describedby="emailHelp"
-                required
               />
+              <div class="invalid-feedback">{{ emailErrorText }}</div>
               <small id="emailHelp" class="form-text text-muted">我們將會使用這個信箱作為聯絡您的方式</small>
             </div>
             <div class="form-group">
-              <label for="mail">建議內容：</label>
+              <label for="content">建議內容：</label>
               <input
                 type="text"
                 class="form-control"
+                :class="contentError?'is-invalid':''"
                 placeholder="想要給本系統改善的建議"
                 id="content"
                 v-model="content"
-                required
               />
+              <div class="invalid-feedback">建議內容不得為空</div>
             </div>
             <button type="submit" class="btn btn-primary float-right">送出</button>
           </form>
@@ -81,7 +83,11 @@ export default {
       mail: "",
       content: "",
       opinionModalShow: false,
-      response: null
+      response: null,
+
+      emailErrorText: "",
+      emailError: false,
+      contentError: false
     };
   },
   methods: {
@@ -96,19 +102,41 @@ export default {
     */
     opinion: function() {
       let vm = this;
-      this.axios
-        .post(ip + "/api/vod/opinion", {
-          mail: this.mail,
-          content: this.content
-        })
-        .then(response => {
-          vm.response = response;
-        })
-        .catch(function(error) {
-          console.log(error);
-        });
-      this.mail = "";
-      this.content = "";
+      let reg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      this.emailError = false;
+      this.contentError = false;
+      this.emailErrorText = "";
+
+      if (this.mail == "" || this.mail == null) {
+        this.emailError = true;
+        this.emailErrorText = "信箱未填寫";
+        if (this.content == "" || this.content == null) {
+          this.contentError = true;
+        }
+      } else if (reg.test(this.mail) == false) {
+        this.emailError = true;
+        this.emailErrorText = "信箱格式不正確";
+        if (this.content == "" || this.content == null) {
+          this.contentError = true;
+        }
+      } else if (this.content == "" || this.content == null) {
+        this.contentError = true;
+      } else {
+        this.axios
+          .post(ip + "/api/vod/opinion", {
+            mail: this.mail,
+            content: this.content
+          })
+          .then(response => {
+            vm.response = response;
+          })
+          .catch(function(error) {
+            console.log(error);
+          });
+        this.opinionModalShow = !this.opinionModalShow;
+        this.mail = "";
+        this.content = "";
+      }
     }
   }
 };
