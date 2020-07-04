96cdca84e48d5c2804ac322cfd454fc6ef363fe6
yoyo <yoyo17813@gmail.com>
2020-07-04 14:07:18 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/clipfetcher/ClipFetcher_Frontend.git master:gh-pages
+git push -f https://github.com/clipfetcher/ClipFetcher_Frontend.git master:gh-master
 
 cd -
