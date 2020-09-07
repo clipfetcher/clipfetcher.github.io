8e9245408a2b1c8507398c134458fdef6f07e928
TsuNiWan <tom007880510@gmail.com>
2020-09-07 19:01:18 +0800

    新增會員登入/註冊/忘記密碼畫面

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,14 @@
 
       <b-collapse id="navbar-toggle-collapse" is-nav>
         <b-navbar-nav class="ml-auto">
+          <b-nav-item>
+            <b-button
+              @click="accountModalShow = !accountModalShow;accountModalTitle = '登入'"
+              size="sm"
+              class="my-2 my-sm-0"
+              variant="info"
+            >Login</b-button>
+          </b-nav-item>
           <b-nav-item to="/home">Vue Home</b-nav-item>
           <b-nav-item to="/axios">Axios</b-nav-item>
           <b-nav-item to="/test">TestComponent</b-nav-item>
@@ -75,7 +83,127 @@
             <button type="submit" class="btn btn-primary float-right">送出</button>
           </form>
         </b-modal>
-        {{ response }}
+
+        <b-modal v-model="accountModalShow" :title="accountModalTitle" hide-footer>
+          <!--登入-->
+          <form v-if="accountModalTitle==='登入'" @submit.prevent="login">
+            <div class="form-group">
+              <label for="loginAccount">帳號：</label>
+              <input
+                type="text"
+                class="form-control"
+                :class="loginAccountError?'is-invalid':''"
+                id="loginAccount"
+                v-model="loginAccount"
+              />
+              <div class="invalid-feedback">{{ loginAccountErrorText }}</div>
+            </div>
+            <div class="form-group">
+              <label for="loginPassword">密碼：</label>
+              <input
+                type="text"
+                class="form-control"
+                :class="loginPasswordError?'is-invalid':''"
+                id="loginPassword"
+                v-model="loginPassword"
+                aria-describedby="loginPasswordHelp"
+              />
+              <div class="invalid-feedback">{{ loginPasswordErrorText }}</div>
+              <small id="loginPasswordHelp" class="form-text">
+                <b-link @click="accountModalTitle='忘記密碼'">忘記密碼</b-link>
+              </small>
+            </div>
+            <div class="form-group">
+              <span>
+                還沒有帳號嗎？
+                <b-link @click="accountModalTitle='註冊'">註冊帳號</b-link>
+              </span>
+              <button type="submit" class="btn btn-primary float-right">登入</button>
+            </div>
+          </form>
+
+          <!--註冊-->
+          <form v-else-if="accountModalTitle==='註冊'" @submit.prevent="signup">
+            <div class="form-group">
+              <span>
+                已經有帳號了嗎？
+                <b-link @click="accountModalTitle='登入'">登入帳號</b-link>
+              </span>
+            </div>
+            <div class="form-group">
+              <label for="signupAccount">帳號：</label>
+              <input
+                type="text"
+                class="form-control"
+                :class="signupAccountError?'is-invalid':''"
+                id="signupAccount"
+                v-model="signupAccount"
+              />
+              <div class="invalid-feedback">{{ signupAccountErrorText }}</div>
+            </div>
+            <div class="form-group">
+              <label for="signupMail">電子信箱：</label>
+              <input
+                type="text"
+                class="form-control"
+                :class="signupMailError?'is-invalid':''"
+                id="signupMail"
+                v-model="signupMail"
+              />
+              <div class="invalid-feedback">{{ signupMailErrorText }}</div>
+            </div>
+            <div class="form-group">
+              <label for="signupPassword">設定密碼</label>
+              <input
+                type="text"
+                class="form-control"
+                :class="signupPasswordError?'is-invalid':''"
+                id="signupPassword"
+                v-model="signupPassword"
+              />
+              <div class="invalid-feedback">{{ signupPasswordErrorText }}</div>
+            </div>
+            <div class="form-group">
+              <label for="signupCheckPassword">確認密碼</label>
+              <input
+                type="text"
+                class="form-control"
+                :class="signupCheckPasswordError?'is-invalid':''"
+                id="signupCheckPassword"
+                v-model="signupCheckPassword"
+              />
+              <div class="invalid-feedback">{{ signupCheckPasswordErrorText }}</div>
+            </div>
+            <div class="form-group">
+              <p class="text-center">
+                點擊註冊及代表您已閱讀並了解
+                <b-link>服務條款</b-link>及
+                <b-link>隱私權聲明</b-link>
+              </p>
+              <p class="text-center">
+                <button type="submit" class="btn btn-primary">註冊</button>
+              </p>
+            </div>
+          </form>
+
+          <!--忘記密碼-->
+          <form v-else-if="accountModalTitle==='忘記密碼'" @submit.prevent="forgotPassword">
+            <div class="form-group">
+              <label for="forgotPasswordId">請輸入帳號或電子信箱：</label>
+              <input
+                type="text"
+                class="form-control"
+                :class="forgotPasswordIdError?'is-invalid':''"
+                id="forgotPasswordId"
+                v-model="forgotPasswordId"
+              />
+              <div class="invalid-feedback">{{ forgotPasswordIdErrorText }}</div>
+            </div>
+            <div class="form-group">
+              <button type="submit" class="btn btn-primary float-right">傳送驗證信件</button>
+            </div>
+          </form>
+        </b-modal>
       </div>
     </footer>
   </div>
@@ -87,14 +215,43 @@ export default {
   data() {
     return {
       title: "Highlight Clip Fetcher",
+      //opinion
+      opinionModalShow: false,
       mail: "",
       content: "",
-      opinionModalShow: false,
-      response: null,
-
       emailErrorText: "",
       emailError: false,
       contentError: false,
+
+      //account
+      accountModalShow: false,
+      accountModalTitle: "",
+      //account-login
+      loginAccount: "",
+      loginPassword: "",
+      loginAccountErrorText: "",
+      loginAccountError: false,
+      loginPasswordErrorText: "",
+      loginPasswordError: false,
+      //account-signup
+      signupAccount: "",
+      signupMail: "",
+      signupPassword: "",
+      signupCheckPassword: "",
+      signupAccountErrorText: "",
+      signupAccountError: false,
+      signupMailErrorText: "",
+      signupMailError: false,
+      signupPasswordErrorText: "",
+      signupPasswordError: false,
+      signupCheckPasswordErrorText: "",
+      signupCheckPasswordError: false,
+      //account-forgotPassword
+      forgotPasswordId: "",
+      forgotPasswordIdErrorText: "",
+      forgotPasswordIdError: false,
+
+      response: null,
     };
   },
   methods: {
@@ -145,6 +302,9 @@ export default {
         this.content = "";
       }
     },
+    login: function () {},
+    signup: function () {},
+    forgotPassword: function () {},
   },
 };
 </script>
