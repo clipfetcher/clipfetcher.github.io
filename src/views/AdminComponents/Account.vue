37b0bcba0c16c2ba1fa5a45c7bfdf34aaa042f3e
TsuNiWan <tom007880510@gmail.com>
2020-10-08 03:05:04 +0800

    串接會員帳號api

--- src/views/AdminComponents/Account.vue
+++ src/views/AdminComponents/Account.vue
@@ -1,12 +1,80 @@
 <template>
   <div>
-    <span>帳號管理頁面</span>
+    <br />
+    <div v-if="accountList === 'Loading'" class="d-flex justify-content-center my-5">
+      <div class="spinner-grow text-secondary" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+    </div>
+    <div v-else-if="accountList === 'Finish'">
+      <b-table striped hover :items="accounts" :fields="fields" responsive="sm">
+        <template v-slot:cell(manage)>
+          <b-button size="sm" class="mr-2">
+            <i class="fas fa-ban"></i> 封鎖用戶
+          </b-button>
+        </template>
+      </b-table>
+    </div>
+    <div v-else class="alert alert-danger" role="alert">
+      <p class="text-center my-2 py-2">
+        <span>系統連線發生錯誤 請等待問題排除或重新整理頁面嘗試 感謝！</span>
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "account",
+  data() {
+    return {
+      fields: [
+        "account",
+        "email",
+        "password",
+        "certification",
+        "role",
+        "manage",
+      ],
+      accountList: "Loading",
+      accounts: null,
+    };
+  },
+  mounted() {
+    this.getContent();
+  },
+  methods: {
+    getContent: function () {
+      let vm = this;
+      this.axios
+        .get(process.env.VUE_APP_ROOT_API + "/api/admin/users", {
+          params: {
+            token: this.$store.state.auth.token,
+          },
+        })
+        .then((response) => {
+          this.accounts = response.data;
+          this.accountList = "Finish";
+        })
+        .catch(function (error) {
+          console.log(error);
+          if (error.response) {
+            if (
+              error.response.status === 402 ||
+              error.response.status === 401 ||
+              error.response.status === 400
+            ) {
+              setTimeout(() => {
+                vm.$router.push({ path: "/" });
+              }, 1000);
+            } else {
+              vm.accountList = "Error";
+            }
+          } else {
+            vm.accountList = "Error";
+          }
+        });
+    },
+  },
+};
 </script>
-
-<style>
-</style>
