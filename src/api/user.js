37c339dbe3c5058e2f4e28ba2bcb12ba5da18f8f
TsuNiWan <47147850+TsuNiWan@users.noreply.github.com>
2020-12-28 00:16:56 +0800

    [修改]API Method修正

--- src/api/user.js
+++ src/api/user.js
@@ -2,7 +2,7 @@ import req from "./http.js"
 
 export const apiUserLogin = params => req("post", "/user/login", params);
 export const apiUserSignup = params => req("post", "/user/signup", params);
-export const apiUserVerify = token => req("post", "/user/verify/" + token);
+export const apiUserVerify = token => req("get", "/user/verify/" + token);
 export const apiUserLogout = params => req("post", "/user/logout", params);
 export const apiUserForgetPassword = params => req("post", "/user/forgetPassword", params);
 export const apiUserUpdatePassword = params => req("post", "/user/updatePassword", params);
