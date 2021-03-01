c890cfd7ff789e2055ece8ce2af9b01aeaf7ba1a
TsuNiWan <47147850+TsuNiWan@users.noreply.github.com>
2021-03-02 02:13:51 +0800

    [更新紀錄]

--- src/views/UpdateLog.vue
+++ src/views/UpdateLog.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="container">
     <h1>更新紀錄</h1>
+    <br />
+    <h2>2021</h2>
+    <hr />
+    <h3>03/02</h3>
+    <ul>
+      <li><h4>新增</h4></li>
+      <p>更新紀錄頁面</p>
+      <li><h4>修改</h4></li>
+      <p>意見回饋改為頁面</p>
+    </ul>
   </div>
 </template>
